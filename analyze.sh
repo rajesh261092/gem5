66d58793ee841567701cad0b779da1f53dca1983
Rajesh Ganesan <grajesh@utexas.edu>
2016-11-19 14:57:49 -0600

    added analyze.sh

--- analyze.sh
+++ analyze.sh
@@ -15,3 +15,19 @@ build/X86/gem5.opt -r --outdir=output/qsort/run1 configs/example/se.py --cmd=ben
 ./simpoint -loadFVFile ../../gem5/output/bitcount/run1/simpoint.bb.gz -maxK 30 -saveSimpoints ../../gem5/output/bitcount/run1/simpoint.out -saveSimpointWeights ../../gem5/output/bitcount/run1/simpoint.weight -inputVectorsGzipped
 ./simpoint -loadFVFile ../../gem5/output/susan/run1/simpoint.bb.gz -maxK 30 -saveSimpoints ../../gem5/output/susan/run1/simpoint.out -saveSimpointWeights ../../gem5/output/susan/run1/simpoint.weight -inputVectorsGzipped
 ./simpoint -loadFVFile ../../gem5/output/qsort/run1/simpoint.bb.gz -maxK 30 -saveSimpoints ../../gem5/output/qsort/run1/simpoint.out -saveSimpointWeights ../../gem5/output/qsort/run1/simpoint.weight -inputVectorsGzipped
+
+
+./simpoint -loadFVFile ../../gem5/output/qsort/run1/simpoint.bb.gz -maxK 1 -saveSimpoints ../../gem5/output/qsort/run1/simpoint_k1.out -saveSimpointWeights ../../gem5/output/qsort/run1/simpoint_k1.weight -inputVectorsGzipped
+./simpoint -loadFVFile ../../gem5/output/qsort/run1/simpoint.bb.gz -maxK 2 -saveSimpoints ../../gem5/output/qsort/run1/simpoint_k2.out -saveSimpointWeights ../../gem5/output/qsort/run1/simpoint_k2.weight -inputVectorsGzipped
+./simpoint -loadFVFile ../../gem5/output/qsort/run1/simpoint.bb.gz -maxK 5 -saveSimpoints ../../gem5/output/qsort/run1/simpoint_k5.out -saveSimpointWeights ../../gem5/output/qsort/run1/simpoint_k5.weight -inputVectorsGzipped
+
+
+./simpoint -loadFVFile ../../gem5/output/susan/run1/simpoint.bb.gz -maxK 1 -saveSimpoints ../../gem5/output/susan/run1/simpoint_k1.out -saveSimpointWeights ../../gem5/output/susan/run1/simpoint_k1.weight -inputVectorsGzipped
+
+
+build/X86/gem5.opt -r --outdir=output/susan/run2 configs/example/se.py --cmd=benchmarks/susan/bin/susan_O2 --options="benchmarks/susan/input_large.pgm output/susan/run2/output_large.smoothing.pgm -s" --take-simpoint-checkpoints="output/susan/run1/simpoint_k1.out,output/susan/run1/simpoint_k1.weight,10000000,0" &
+build/X86/gem5.opt -r --outdir=output/susan/run3 configs/example/se.py --cmd=benchmarks/susan/bin/susan_O2 --options="benchmarks/susan/input_large.pgm output/susan/run3/output_large.smoothing.pgm -s" --take-simpoint-checkpoints="output/susan/run1/simpoint_k1.out,output/susan/run1/simpoint_k1.weight,10000000,1000000" &
+build/X86/gem5.opt -r --outdir=output/susan/run4 configs/example/se.py --cmd=benchmarks/susan/bin/susan_O2 --options="benchmarks/susan/input_large.pgm output/susan/run4/output_large.smoothing.pgm -s" --take-simpoint-checkpoints="output/susan/run1/simpoint.out,output/susan/run1/simpoint.weight,10000000,1000000" &
+
+
+build/X86/gem5.opt -r --outdir=output/susan/run5 configs/example/se.py --cmd=benchmarks/susan/bin/susan_O2 --options="benchmarks/susan/input_large.pgm output/susan/run5/output_large.smoothing.pgm -s" --restore-simpoint-checkpoint -r 1 --checkpoint-dir="output/susan/run2/cpt.simpoint_00_inst_140000000_weight_1.000000_interval_10000000_warmup_0" &
